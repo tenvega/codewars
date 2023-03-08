3e8915f7bf8fcef8141f365643571ef034b6d6cb
tenvega <etorra@gmail.com>
2023-03-08 12:28:51 +0500

    Write function RemoveExclamationMarks which removes all exclamation marks from a given string

---
+++ 8_kyu/hellow_world.js
@@ -0,0 +1,9 @@
+//Make a simple function called greet that returns the most-famous "hello world!".
+
+function greet() {
+    let greetings = "Hello World!";
+    return greetings.toLowerCase()
+}
+
+
+console.log(greet())
---
+++ 8_kyu/remove_exclamation_mark.js
@@ -0,0 +1,17 @@
+//Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+
+function removeExclamationMarks(s) {
+    let result = ""
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] !== "!") {
+            result += s[i]
+        }
+    }
+    return result
+
+}
+
+console.log(removeExclamationMarks("Hello World!"))
+
+
