b425bddc89fea5d767d044cd6d9b73e55c98a6f8
tenvega <etorra@gmail.com>
2023-03-12 08:59:31 +0500

    Implement a function which convert the given boolean value into its string representation

---
+++ 8_kyu/convert_boolean_to_a_string.js
@@ -0,0 +1,12 @@
+//Implement a function which convert the given boolean value into its string representation.
+
+function booleanToString(b) {
+    if (b === true) {
+        return "true"
+    } else {
+        return "false"
+    }
+}
+
+console.log(booleanToString(true))
+console.log(booleanToString(false))
