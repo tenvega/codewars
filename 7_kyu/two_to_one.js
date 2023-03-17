50ae5a15ef960c4a6f0358233870908ff951d84e
tenvega <etorra@gmail.com>
2023-03-17 09:23:21 +0500

    Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2

---
+++ 7_kyu/two_to_one.js
@@ -0,0 +1,18 @@
+//Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+function longest(s1, s2) {
+    let uniqueChars = new Set()
+    for (let i = 0; i < s1.length; i++) {
+        uniqueChars.add(s1.charAt(i))
+    }
+    for (let j = 0; j < s2.length; j++) {
+        uniqueChars.add(s2.charAt(j))
+    }
+    let sortedChars = Array.from(uniqueChars).sort()
+    let combined = sortedChars.join("")
+    return combined
+}
+
+console.log(longest("aretheyhere", "yestheyarehere"))
+console.log(longest("loopingisfunbutdangerous", "lessdangerousthancoding"))
+console.log(longest("inmanylanguages", "theresapairoffunctions"))
