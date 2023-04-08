8efeaaec0301cf630b518f269bb561b161fba3c1
tenvega <etorra@gmail.com>
2023-04-08 17:23:28 -0400

    Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive )

---
+++ 8_kyu/power_of_two.js
@@ -0,0 +1,14 @@
+// Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).
+
+function powersOfTwo(n) {
+  var result = [];
+  
+  for (var i = 0; i <= n; i++) {
+    result.push(Math.pow(2, i));
+  }
+  
+  return result;
+}
+
+var powers = powersOfTwo(4);
+console.log(powers); // Output: [1, 2, 4, 8, 16, 32]
